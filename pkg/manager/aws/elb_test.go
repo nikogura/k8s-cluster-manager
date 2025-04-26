6a7811c3d3a249e1b07f7dbfe18cd3d1f6ad08b0
no <no@no.com>
2025-04-25 22:14:31 -0700

    First mock example

--- pkg/manager/aws/elb_test.go
+++ pkg/manager/aws/elb_test.go
@@ -1,7 +1,11 @@
 package aws
 
 import (
+	"github.com/google/go-cmp/cmp"
+	"github.com/nikogura/k8s-cluster-manager/pkg/manager"
 	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -61,5 +65,69 @@ func TestLoadBalancerName(t *testing.T) {
 			t.Errorf("failed generating lb name: %s", err)
 		}
 		assert.Equal(t, tc.expected, actual, "actual load balancer name does not meet expectations")
+	}
+}
+
+type AWSClusterManager_GetClusterLBsAPI interface {
+	GetClusterLBs() (lbs []manager.LBInfo, err error)
+}
+
+func AWSClusterManager_GetClusterLBs(api AWSClusterManager_GetClusterLBsAPI) ([]manager.LBInfo, error) {
+	object, err := api.GetClusterLBs()
+	if err != nil {
+		return nil, err
+	}
+
+	return object, err
+}
+
+type mockAWSClusterManager_GetClusterLBsAPI func() (lbs []manager.LBInfo, err error)
+
+func (m mockAWSClusterManager_GetClusterLBsAPI) GetClusterLBs() (lbs []manager.LBInfo, err error) {
+	return m()
+}
+
+func TestAWSClusterManager_GetClusterLBs(t *testing.T) {
+	testCases := []struct {
+		name   string
+		client func(t *testing.T) AWSClusterManager_GetClusterLBsAPI
+		expect []manager.LBInfo
+	}{
+		{
+			name: "test",
+			client: func(t *testing.T) AWSClusterManager_GetClusterLBsAPI {
+				return mockAWSClusterManager_GetClusterLBsAPI(
+					func() (lbs []manager.LBInfo, err error) {
+						t.Helper()
+						return []manager.LBInfo{
+							{
+								Name:        "TestLB",
+								IsApiServer: false,
+								Targets:     []manager.LBTargetInfo{},
+							},
+						}, err
+					},
+				)
+			},
+			expect: []manager.LBInfo{
+				{
+					Name:        "TestLB",
+					IsApiServer: false,
+					Targets:     []manager.LBTargetInfo{},
+				},
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strings.Join([]string{strconv.Itoa(i), tc.name}, ". "), func(t *testing.T) {
+			got, err := AWSClusterManager_GetClusterLBs(tc.client(t))
+			if err != nil {
+				t.Fatalf("no error expected with mock, got %v", err)
+			}
+			if e, g := tc.expect, got; cmp.Equal(e, g) != true {
+				t.Errorf("expect %v, got %v", e, g)
+			}
+		})
 	}
 }
